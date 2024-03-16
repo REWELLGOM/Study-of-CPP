6ecdd76f6b62064978c7098acf1adbc5dc5ee06f
seong mok lee <deadmouse210@naver.com>
2024-03-16 11:46:18 +0900

    1-1test

---
+++ Theory/Class_1/1-1/test1_1.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+int main(void)
+{
+	/*int val1;
+	int val2;
+	int val3;
+	int val4;
+	int val5;*/
+
+	/*std::cout << "1번째 정수입력: ";
+	std::cin >> val1;
+
+	std::cout << "2번째 정수입력: ";
+	std::cin >> val2;
+
+	std::cout << "3번째 정수입력: ";
+	std::cin >> val3;
+
+	std::cout << "4번째 정수입력: ";
+	std::cin >> val4;
+
+	std::cout << "5번째 정수입력: ";
+	std::cin >> val5;
+
+	int result = val1 + val2 + val3 + val4 + val5;
+
+	std::cout << result;*/
+
+	/*char name[100]; //1-1 문제2
+	char Pnum[100];
+
+	std::cin >> name;
+	std::cin >> Pnum;
+
+	std::cout << "내 이름: " << name << std::endl;
+	std::cout << "폰 번호: " << Pnum << std::endl;*/
+
+	/*int num; //1-1 문제3
+	int result = 0;
+	std::cin >> num;
+	for (int i = 1; i < 10; i++) {
+		std::cout << "구구단 결과: " << num*i << std::endl;
+	}*/
+
+	//int money=0; //1-1 문제4
+	//int sellnum=0;
+	//float result=0;
+	//while (1) 
+	//{
+	//	std::cout << "판매금액: ";
+	//	std::cin >> money;
+	//	result = money * 0.12f + 50;
+	//	std::cout << "판매 금액을 만원 단위로 입력(-1 to end)" << money << std::endl;
+	//	if (money < 0) {
+	//		std::cout << "프로그램을 종료함" << std::endl;
+	//		break;
+	//	}
+	//	std::cout <<"이번 달 급여" << result << std::endl;
+	//}
+}
