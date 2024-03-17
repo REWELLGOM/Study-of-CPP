3017f5bd2f7447b8ad479f21325603edbbaac2ff
seong mok lee <deadmouse210@naver.com>
2024-03-17 22:13:35 +0900

    line_func

--- Theory/Class_1/Inline_Func/Prac.cpp
+++ Theory/Class_1/Inline_Func/Prac.cpp
@@ -1,5 +1,6 @@
 //메크로 함수라고 생각하면 좀 이해하기 쉬울거 같다
 
+
 #include <iostream>
 
 using namespace std;
@@ -14,3 +15,23 @@ int main(void)
     cout<<SQUARE(12)<<endl;
     return 0;
 }
+
+
+/*
+#include <string>
+using namespace std;
+
+
+string solution(int num) {
+    num =3;
+    string answer = "";
+    if(num/2 == 0)
+    {
+        answer = "Even";
+    }else
+    {
+        answer = "Odd";
+    }
+    return answer;
+}
+*/
