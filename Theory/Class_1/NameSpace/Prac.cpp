bb5e058b5f6a83448c85cfc6f9b1b96cf648006f
seong mok lee <deadmouse210@naver.com>
2024-03-18 13:04:47 +0900

    NameSpace_Prac

--- Theory/Class_1/NameSpace/Prac.cpp
+++ Theory/Class_1/NameSpace/Prac.cpp
@@ -24,6 +24,7 @@ int main(void)
 }
 */
 
+/*
 namespace BestComImpl
 {
     void SimpleFunc(void);
@@ -50,3 +51,45 @@ void ProgComImpl::SimpleFunc(void)
 {
     std::cout<<"ProgCom이 정의한 함수"<<std::endl;
 }
+*/
+
+/*
+namespace BestComImpl
+{
+    void SimpleFunc(void);
+}
+
+namespace BestComImpl
+{
+    void PrettyFunc(void);
+}
+
+namespace ProgComImpl
+{
+    void SimpleFunc(void);
+}
+
+int main(void)
+{
+    BestComImpl::SimpleFunc();
+    return 0;
+}
+
+void BestComImpl::SimpleFunc(void)
+{
+    std::cout<<"BestCom이 정의한 함수"<<std::endl;
+    PrettyFunc();
+    ProgComImpl::SimpleFunc();
+}
+
+void BestComImpl::PrettyFunc(void)
+{
+    std::cout<<"So Pretty!!"<<std::endl;
+}
+
+void ProgComImpl::SimpleFunc()
+{
+    std::cout<<"ProgCom이 정의한 함수"<<std::endl;
+}
+*/
+
