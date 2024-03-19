df7279513d805d6663f44ea73dae7afc5a76145d
seong mok lee <deadmouse210@naver.com>
2024-03-19 21:55:41 +0900

    tutorial

---
+++ lecture/1_2/Input_Output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    int numOfWatermelon = 0;
+    cout<<"How many watermelons do you have? "<<endl;
+    cin >> numOfWatermelon;
+
+    cout << "I have "<<numOfWatermelon;
+    return 0;
+}
---
+++ lecture/1_2/variable.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    int age = 10;
+    cout<<"My age is "<<age<<endl;
+    return 0;
+}
