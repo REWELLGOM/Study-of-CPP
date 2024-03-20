81efa4f01b730334667f6cf3110e962a8d8c9bba
seong mok lee <deadmouse210@naver.com>
2024-03-20 16:24:09 +0900

    multiplication_table

--- lecture/1-5/multiplication_table.cpp
+++ lecture/1-5/multiplication_table.cpp
@@ -4,13 +4,16 @@ int main(void)
 {
     using namespace std;
     int num = 0;
+    cout<<"몇 단: ";
     cin>>num;
     cout<<num<<"단 시작"<<endl;
     int result = 0;
 
     for(int i = 1; i<10;i++)
     {
-        
+        result = num * i;
+        cout<<num<<" * "<<i<<" = "<<result<<endl;
     }
+    return 0;
 
 }
