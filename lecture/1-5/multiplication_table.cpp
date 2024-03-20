b18c4f504ea08104dad8e19ebcaefdf41c187d6f
seong mok lee <deadmouse210@naver.com>
2024-03-20 16:21:11 +0900

    change_file_name

---
+++ lecture/1-5/multiplication_table.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(void)
+{
+    using namespace std;
+    int num = 0;
+    cin>>num;
+    cout<<num<<"단 시작"<<endl;
+    int result = 0;
+
+    for(int i = 1; i<10;i++)
+    {
+        
+    }
+
+}
