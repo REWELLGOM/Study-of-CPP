f19051ebdf9c7d79058ed351c68c4d6b8c199542
seong mok lee <deadmouse210@naver.com>
2024-03-27 00:10:58 +0900

    quiz2_fail

--- Theory/Class_2/Reference_and_Function/quiz2.cpp
+++ Theory/Class_2/Reference_and_Function/quiz2.cpp
@@ -1,8 +1,9 @@
+/*
 #include <iostream>
 
 using namespace std;
 
-int SwapPointer(&ptr1, &ptr2)
+int SwapPointer(&ptr1, &ptr2) // 포인터를 매개변수로 받아야 하므로, 타입 뒤에 *를 추가하고 반환 타입을 void로 변경해야함
 {
     int num1 = ptr1;
     ptr1 = ptr2;
@@ -17,4 +18,30 @@ int main(void)
     int *ptr2 = &num2;
 
     SwapPointer(*ptr1, *ptr2)
+}
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+void SwapPointer(int* &ptr1, int* &ptr2)
+{
+    int* temp = ptr1;
+    ptr1 = ptr2;
+    ptr2 = temp;
+}
+
+int main(void)
+{
+    int num1 = 5;
+    int *ptr1 = &num1;
+    int num2 = 10;
+    int *ptr2 = &num2;
+
+    SwapPointer(ptr1, ptr2);
+
+    // 결과 확인을 위한 출력
+    cout << "ptr1 가리키는 값: " << *ptr1 << ", ptr2 가리키는 값: " << *ptr2 << endl;
 }
