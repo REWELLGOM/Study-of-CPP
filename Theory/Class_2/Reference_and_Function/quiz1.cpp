6540314ef076eb3153bce9e133597586955e265c
seong mok lee <deadmouse210@naver.com>
2024-03-26 23:34:50 +0900

    quiz_fail

--- Theory/Class_2/Reference_and_Function/quiz1.cpp
+++ Theory/Class_2/Reference_and_Function/quiz1.cpp
@@ -1,10 +1,11 @@
+/*
 #include <iostream>
 
 using namespace std;
 
-void increaseFunc(int &num)
+void increaseFunc(int &a)
 {
-    num++;
+    a++;
     transFunc(num);
 }
 
@@ -19,6 +20,34 @@ int main(void)
     int num = 0;
     cout<<"변환하고자하는 값: ";
     cin>>num;
+
+    increaseFunc(num);
+}
+*/
+
+#include <iostream>
+
+using namespace std;
+
+// transFunc 함수를 먼저 정의
+int transFunc(int a)
+{
+    return a * (-1);
+}
+
+// 이제 transFunc 함수 프로토타입 선언 없이도 increaseFunc 내에서 transFunc 호출 가능
+void increaseFunc(int &a)
+{
+    a++;
+    int result = transFunc(a);
+    cout << "변환 후 값: " << result << endl;
+}
+
+int main(void)
+{
+    int num = 0;
+    cout << "변환하고자하는 값: ";
+    cin >> num;
 
     increaseFunc(num);
 }
