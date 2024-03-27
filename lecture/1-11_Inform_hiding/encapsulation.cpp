5646de947345e53682253fea8207298fd3eed1b7
seong mok lee <deadmouse210@naver.com>
2024-03-27 22:44:33 +0900

    encapsulation

--- lecture/1-11_Inform_hiding/encapsulation.cpp
+++ lecture/1-11_Inform_hiding/encapsulation.cpp
@@ -8,7 +8,7 @@ class Person
 private:
     string m_name;
 
-public:
+public:     //public안쓰면 기본적으로 private임
     Person(string name)
     {
         m_name = name;
@@ -16,16 +16,16 @@ public:
 
     void sayHello()
     {
-        cout << "Hello I'm " << m_name << endl;
+        cout << "Hello I'm " << m_name << endl; 
     }
 
     string getName()
     {
-        return m_name;
+        return m_name; //private에 접근하기위한 메소드 = 중개자 역할
     }
 };
 
-int main()
+int main()  //main에서 m_name을 수정할 방법이 없음
 {
     Person p1 = Person("Eric Clapton");
     p1.sayHello();
