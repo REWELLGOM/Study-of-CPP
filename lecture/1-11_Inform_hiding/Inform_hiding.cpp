287df47d15caf97c1191efa4ccc2903af869b5c4
seong mok lee <deadmouse210@naver.com>
2024-03-27 22:40:59 +0900

    private

--- lecture/1-11_Inform_hiding/Inform_hiding.cpp
+++ lecture/1-11_Inform_hiding/Inform_hiding.cpp
@@ -16,14 +16,14 @@ public:
 
     void sayHello()
     {
-        cout << "Hello I'm " << m_name << endl;
+        cout << "Hello I'm " << m_name << endl;  
     }
 };
 
 int main()
 {
     Person p1 = Person("Eric Clapton");
-    p1.sayHello();
+    p1.sayHello();  //클래스 내부에있는 메소드들은 private에 접속가능
 	//cout << p1.m_name << endl; //private으로 선언되어서 공개가안됌
 
     return 0;
