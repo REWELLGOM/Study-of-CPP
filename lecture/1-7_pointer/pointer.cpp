07b2c79b8c5f30fe49a09b40f1b1cc75627ea5bd
seong mok lee <deadmouse210@naver.com>
2024-03-29 22:55:31 +0900

    none

--- lecture/1-7_pointer/pointer.cpp
+++ lecture/1-7_pointer/pointer.cpp
@@ -5,7 +5,7 @@ int main()
     using namespace std;
 
     int watermelons = 10;
-    int *p_watermelonBox = &watermelons; //포인터 변수를 선언 변수의 주소를 가져옴 * , &는 세트임
+    int *p_watermelonBox = &watermelons; // 포인터 변수를 선언 변수의 주소를 가져옴 * , &는 세트임
     //*p_watermelonBox = &watermelons 포인터 변수의 값을 가져오라는 말임 이 코드는 틀린거임
 
     cout << "watermelons: " << watermelons << endl;
@@ -15,5 +15,5 @@ int main()
     cout << "p_watermelonBox value: " << p_watermelonBox << endl;
 
     *p_watermelonBox = *p_watermelonBox + 1;
-    cout << "Now I have " << watermelons << endl; //주소가 같아서
+    cout << "Now I have " << watermelons << endl; // 주소가 같아서
 }
