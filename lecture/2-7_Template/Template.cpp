9973e7e4db80c9c1dc7f528f515d39a7235a8f6b
seong mok lee <deadmouse210@naver.com>
2024-03-30 23:30:05 +0900

    template

--- lecture/2-7_Template/Template.cpp
+++ lecture/2-7_Template/Template.cpp
@@ -3,17 +3,17 @@
 
 using namespace std;
 
-int sum(int a, int b)
-{
-    return a + b;
-}
+// int sum(int a, int b)
+// {
+//     return a + b;
+// }
 
-double sum(double a, double b)
-{
-    return a + b;
-}
+// double sum(double a, double b)
+// {
+//     return a + b;
+// }
 
-template <typename T>
+template <typename T>  //예외처리가 필요함
 T sum(T a, T b)
 {
     return a + b;
@@ -25,11 +25,13 @@ int main()
     double s2 = sum(2.2, 2.0);
     string s3 = sum<string>("Sparta", "CodingClub");
     double s4 = sum<double>(2.2, 2.0);
+    char s5 = sum<char>('a','b');  //원하는 ab가 안나옴
 
     cout << s1 << endl;
     cout << s2 << endl;
     cout << s3 << endl;
     cout << s4 << endl;
+    cout << s5 << endl;
 
     return 0;
 }
