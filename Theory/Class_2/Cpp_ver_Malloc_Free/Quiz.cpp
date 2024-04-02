35c9876e882afdd4af97f1335c4260ef63322aab
seong mok lee <deadmouse210@naver.com>
2024-04-02 21:01:57 +0900

    quiz

--- Theory/Class_2/Cpp_ver_Malloc_Free/Quiz.cpp
+++ Theory/Class_2/Cpp_ver_Malloc_Free/Quiz.cpp
@@ -7,4 +7,14 @@ typedef struct __point
     int xpos;
     int ypos;
 
+    int PntAdder()
+    {
+        
+    }
+
 } Point;
+
+int main (void)
+{
+    Point& PntAdder(const Point &p1, const Point &p2);
+}
