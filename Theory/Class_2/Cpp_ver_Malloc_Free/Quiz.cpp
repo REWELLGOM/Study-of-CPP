de506b4376aad07e4ec8d68818dcd49f61d4348b
seong mok lee <deadmouse210@naver.com>
2024-04-02 23:53:51 +0900

    quiz

--- Theory/Class_2/Cpp_ver_Malloc_Free/Quiz.cpp
+++ Theory/Class_2/Cpp_ver_Malloc_Free/Quiz.cpp
@@ -7,14 +7,25 @@ typedef struct __point
     int xpos;
     int ypos;
 
-    int PntAdder()
+public:
+    float PntAdder(const float *p1, const float *p2)
     {
-        
+        float result = 0;
+        result = *p1 + *p2;
+        cout<<result;
+        return result;
     }
 
 } Point;
 
 int main (void)
 {
+    int p1 = 0;
+    int p2 = 0;
+
+    cout<<"p1: ";
+    cin>>p1;
+    cout<<"p2: ";
+    cin>>p2;
     Point& PntAdder(const Point &p1, const Point &p2);
 }
