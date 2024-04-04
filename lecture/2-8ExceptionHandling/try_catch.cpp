ef18fcc433549ff3a5598a027753b267c833cbb2
LeeSeongMok <deadmouse210@naver.com>
2024-04-04 23:29:31 +0900

    try_catch

--- lecture/2-8ExceptionHandling/try_catch.cpp
+++ lecture/2-8ExceptionHandling/try_catch.cpp
@@ -3,5 +3,29 @@ using namespace std;
 
 int main()
 {
+    int number[3] = {1,3,5};
+    int input = 0;
+
+    cout <<"Enter integer number(1~3)"<<endl;
+    cin >> input;
+
+    if(input < 1 || input > 3)
+    {
+        cout << "Error!"<<endl;
+        return 0;
+    }
     
+    try
+    {
+        if(input < 1 || input > 3)
+        {
+            throw input;         //throw는 일단 먼저 시도하겠다는 말
+        }
+        cout << number[input - 1]<<endl;
+    } catch(int e)   //throw에서 날린걸 e라는걸로 잡음   input -> e
+    {
+        cout << "Invalid input: "<<e<<endl;
+    }
+    cout << "Done!"<<endl;
+    return 0;
 }
