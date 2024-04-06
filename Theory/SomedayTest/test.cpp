570f34746246937c4d06eee55410f591340d3cae
LeeSeongMok <deadmouse210@naver.com>
2024-04-07 01:09:52 +0900

    test

--- Theory/SomedayTest/test.cpp
+++ Theory/SomedayTest/test.cpp
@@ -33,8 +33,31 @@ int main(void)
     return 0;
 }
 */
+int BoxVal(int length, int width, int height)
+{
+    cout<<"int로 왔다"<<endl;
+    return length*width*height;
+}
+
+float BoxVal(float length, float width, float height)
+{
+    cout<<"float으로 왔다"<<endl;
+    return length*width*height;
+}
+
+int BoxVal()
+{
+    cout<<"여기도 들어왔찌롱"<<endl;
+    return 0;
+}
 
 int main()
 {
-    
+    int a = 0, b = 0, c = 0;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cout<<a<<endl<<b<<endl<<c<<endl;
+    cout<<"result = "<<BoxVal(a,b,c)<<endl;
+    return 0;
 }
