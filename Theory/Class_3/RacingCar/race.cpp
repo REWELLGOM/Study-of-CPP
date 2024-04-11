24f9d456ea97083c30f7040b6959211352ead1a4
LeeSeongMok <deadmouse210@naver.com>
2024-04-11 13:58:22 +0900

    Ongoing

---
+++ Theory/Class_3/RacingCar/race.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+#define ID_LEN      20
+#define MAX_SRP     200
+#define FUEL_STEP   2
+#define ACC_STEP    10
+#define BRK_STEP    10
+
+struct Car
+{
+    char gamerID[ID_LEN];
+    int fuelGauge;
+    int curSpeed;
+
+};
+
+void ShowCarState(const Car &car)
+{
+    cout<<"소유자 ID: "<<car.gamerID<<endl;
+    cout<<"연료량: "<<car.fuelGauge<<endl;
+    cout<<"현재속도: "<<car.curSpeed<<endl;
+}
+
+void Accel(Car &car)
+{
+    if(car.fuelGauge<=0)
+    {
+        return;
+    }
+    else
+    {
+        car.fuelGuage -=FUEL_STEP;
+    }
+
+    if(car.curSpeed+)
+}
