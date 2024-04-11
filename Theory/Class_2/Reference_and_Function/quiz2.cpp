a2250e42c8b77a30a53a559f75ed230fae15a4ed
LeeSeongMok <deadmouse210@naver.com>
2024-04-11 13:58:14 +0900

    Error

--- Theory/Class_2/Reference_and_Function/quiz2.cpp
+++ Theory/Class_2/Reference_and_Function/quiz2.cpp
@@ -1,9 +1,8 @@
-/*
 #include <iostream>
-
 using namespace std;
 
-int SwapPointer(&ptr1, &ptr2) // 포인터를 매개변수로 받아야 하므로, 타입 뒤에 *를 추가하고 반환 타입을 void로 변경해야함
+int SwapPointer(&ptr1, &ptr2) 
+// 포인터를 매개변수로 받아야 하므로, 타입 뒤에 *를 추가하고 반환 타입을 void로 변경해야함
 {
     int num1 = ptr1;
     ptr1 = ptr2;
@@ -19,8 +18,9 @@ int main(void)
 
     SwapPointer(*ptr1, *ptr2)
 }
-*/
 
+
+/*
 #include <iostream>
 
 using namespace std;
@@ -45,3 +45,4 @@ int main(void)
     // 결과 확인을 위한 출력
     cout << "ptr1 가리키는 값: " << *ptr1 << ", ptr2 가리키는 값: " << *ptr2 << endl;
 }
+*/
