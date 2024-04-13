c41c381e01c1563b63fb392d8eca2c4ce70c3c8b
LeeSeongMok <deadmouse210@naver.com>
2024-04-13 14:28:17 +0900

    Add_Explain

--- Theory/Class_3/RacingCar/Quiz3_1.cpp
+++ Theory/Class_3/RacingCar/Quiz3_1.cpp
@@ -16,7 +16,10 @@ struct Vector
 
     void AddPoint(const Vector &pos)
     {
-        ;
+        //pos2에 박아넣은 값중에서 xpos ypos 값 추출해서 더하기
+        xpos += pos.xpos;
+        ypos += pos.ypos;
+        return ;
     }
 
     void ShowPosition()
@@ -31,8 +34,10 @@ int main ()
 {
     Vector pos1 = {12, 4};
     Vector pos2 = {20, 30};
+    //각각 xpos ypos에 값 넣기
 
     pos1.MovePos(-7, 10);
+    //Vector구조체에 pos1을 집어넣음 그와동시에 MovePos로 값 깎기
     pos1.ShowPosition();
 
     pos1.AddPoint(pos2);
