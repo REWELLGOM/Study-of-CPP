bab61b1e4216d618dc1d61709100ebfb3b065b4f
MOK <deadmouse210@naver.com>
2024-04-18 23:41:33 +0900

    Class

--- Theory/Class_4/Class/Class.cpp
+++ Theory/Class_4/Class/Class.cpp
@@ -1,2 +1,34 @@
 #include <iostream>
 using namespace std;
+
+class Point
+{
+public:
+    int x;
+    int y;
+};
+
+class Rectangle
+{
+public:
+    Point upleft;
+    Point lowRight;
+
+public:
+    void ShowRecInfo()
+    {
+        cout<<"좌 상단: "<<'['<<upleft.x<<", ";
+        cout<<upleft.y<<']'<<endl;
+        cout<<"우 하단: "<<'['<<lowRight.x<<", ";
+        cout<<lowRight.y<<']'<<endl<<endl;
+    }
+};
+
+int main(void)
+{
+    Point pos1 = {-2, 4};
+    Point pos2 = {5, 6};
+    Rectangle rec = {pos2, pos1};
+    rec.ShowRecInfo();
+    return 0;
+}
