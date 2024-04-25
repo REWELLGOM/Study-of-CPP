0f33389e7239f0918e768ed181845823019b7de4
MOK <deadmouse210@naver.com>
2024-04-25 12:33:33 +0900

    none

--- Theory/Quiz/Quiz4_2.cpp
+++ Theory/Quiz/Quiz4_2.cpp
@@ -2,32 +2,64 @@
     다시해봐 main과 point클래스 건들지말고 circle ring클래스가 이어지게 해서 도출하시오
 */
 
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// class Point
-// {
-// private:
-//     int xpos, ypos;
+class Point
+{
+private:
+    int xpos, ypos;
 
-// public:
-//     void Init(int x,int y)
-//     {
-//         xpos = x;
-//         ypos = y;
-//     }
-//     void ShowPointInfo() const
-//     {
-//         cout<<"["<<xpos<<", "<<ypos<<"]"<<endl;
+public:
+    void Init(int x,int y)
+    {
+        xpos = x;
+        ypos = y;
+    }
+    void ShowPointInfo() const
+    {
+        cout<<"["<<xpos<<", "<<ypos<<"]"<<endl;
 
-//     }
+    }
 
-// };
+};
 
-// int main(void)
-// {
-//     Ring ring;
-//     ring.Init(1,1,4,2,2,9);
-//     ring.ShowRingInfo();
-//     return 0;
-// }
+
+class Circle
+{
+private:
+    Point xpos;
+    Point ypos;
+public:
+    void Init()
+    {
+
+    }
+};
+
+
+class Ring
+{
+private:
+    Circle OutRadius;
+    Circle InRadius;
+    int x,y,r,x2,y2,r2;
+public:
+    Circle Init(int x,int y,int r,int x2,int y2,int r2)
+    {
+        
+    }
+
+    void ShowRingInfo()
+    {
+        cout<<r2 - r<<endl;
+    }
+};
+
+int main(void)
+{
+    Ring ring;
+    ring.Init(1,1,4,2,2,9);
+    ring.ShowRingInfo();
+    return 0;
+}
